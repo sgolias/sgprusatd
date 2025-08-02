73eef1463990c161d20a2c5c162ae8bc326ac0dc
sgolias <s.golias@seznam.cz>
2025-08-02 20:36:33 +0000

    Spuštěný tomcat v app

---
+++ sgprusatd/src/main/java/cz/sg_example/sgprusatd/SgprusatdApplication.java
@@ -0,0 +1,13 @@
+package cz.sg_example.sgprusatd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgprusatdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgprusatdApplication.class, args);
+	}
+
+}
---
+++ sgprusatd/src/test/java/cz/sg_example/sgprusatd/SgprusatdApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.sg_example.sgprusatd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgprusatdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
